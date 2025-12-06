eee8343fdfc10586e7b88113e075d0947b4ac961
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2025-12-06 13:31:45 -0700

    build: updated storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,24 +3,16 @@ const config: StorybookConfig = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 	addons: [
 		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-interactions",
-		"@storybook/addon-styling",
-		{
-			name: "@storybook/addon-styling",
-			options: {},
-		},
-		{
-			name: "@storybook/addon-styling",
-			options: {},
-		},
+		"@storybook/addon-themes",
+		"@storybook/addon-docs",
 	],
 	framework: {
 		name: "@storybook/react-vite",
 		options: {},
 	},
 	docs: {
-		autodocs: "tag",
+		defaultName: "Documentation",
+		docsMode: true,
 	},
 };
 export default config;
